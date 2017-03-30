f71cdd2c8d6bac01923c5827c3a0c4ffba8a4f21
Shreyap16 <spatel429@yahoo.com>
2017-03-30 18:09:52 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function WordCount(str) {var totalSoFar = 0; for (var i = 0; i < WordCount.length; i++)
+    if (str(i) === " ") {
+      totalSoFar = +1;}
+  totalsoFar += 1;}
+
+console.log(WordCount("Random String"));
+
+var wordAvg = 0;
+  for (var i = 0; i < wordCount.length; i++){
+    wordAvg += wordArray[i];} var avgLen = wordAvg / wordCount;
+    console.log(("Words: " + wordArray[0]), "Character count: " + charCount, "Word count: " + wordCount, "Whitespace count: " + whiteSpace, "Word length average: " + avgLen);};
+
+    str.length - str.replace(/\s+/g, '').length;
